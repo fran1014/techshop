a43352fc00846b8eb491b14875b615b3c6967d95
Franklin <71532004+fran1014@users.noreply.github.com>
2023-07-22 01:28:45 +0200

    creation of headers

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return <header>Header
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect
+  </header>;
+};
+
+export default Header;
