4ea494b6f3be6a119c6c5b8b6bcd4777454d495e
Franklin <71532004+fran1014@users.noreply.github.com>
2023-07-22 11:20:22 +0200

    heading structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,29 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
 const Header = () => {
-  return <header>Header
-    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect
-  </header>;
+  return (
+    <header>
+      Header
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">TechShop</Navbar.Brand>
+          <Navbar.Toggle arialcontrols="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser />
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
