986b4086f7bc215ca51951416ca21d7c2289df62
Franklin <71532004+fran1014@users.noreply.github.com>
2023-07-30 11:04:58 +0200

    auth and login working

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
+    ? JSON.parse[localStorage.getItem('userInfo')]
     : null,
 };
 
@@ -13,6 +13,10 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
+    },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem('userInfo');
     },
   },
 });
